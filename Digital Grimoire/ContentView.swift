91f7f93a720b1e764b848662f8ff965a5dae0a3b
Knifin <angie.t.seattle@gmail.com>
2024-10-04 14:36:24 -0700

    Add files.

---
+++ Digital Grimoire/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Digital Grimoire
+//
+
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: "https://app.shinpostudios.com/")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let _view: WKWebView
+    let _url: String
+    
+    init(url: String) {
+        _view = WKWebView(frame: .zero)
+        _url = url
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return _view
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        _view.load(URLRequest(url: URL(string: _url)!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 1))
+    }
+}
---
+++ Digital Grimoire/Digital_GrimoireApp.swift
@@ -0,0 +1,15 @@
+//
+//  Digital_GrimoireApp.swift
+//  Digital Grimoire
+//
+
+import SwiftUI
+
+@main
+struct Digital_GrimoireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
